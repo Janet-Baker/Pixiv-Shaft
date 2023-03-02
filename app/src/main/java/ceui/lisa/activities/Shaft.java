7e4b53c0e59fc7a8903d757d6a7dc8d5428e219e
Janet Baker <guoxiangyuan1@gmail.com>
2023-03-02 16:28:11 +0800

    Fit my usage
    modify build.gradle to pass compile.
    Remove Weibo and rating.
    Remove google services.
    Remove google Firebase.
    Rename app to "MajSoul".
    Remove Permission "android.permission.READ_PHONE_STATE".
    Enable SNI fronting by default.
    update repositories

--- app/src/main/java/ceui/lisa/activities/Shaft.java
+++ app/src/main/java/ceui/lisa/activities/Shaft.java
@@ -12,7 +12,6 @@ import android.net.ConnectivityManager;
 import android.view.Gravity;
 
 import com.billy.android.swipe.SmartSwipeBack;
-import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.gson.Gson;
 import com.hjq.toast.ToastUtils;
 
@@ -118,9 +117,6 @@ public class Shaft extends Application implements ServicesProvider {
         ToastUtils.setGravity(Gravity.BOTTOM, 0, 0);
         ToastUtils.initStyle(new ToastStyle(this));
 
-        FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(
-                sSettings.isFirebaseEnable()
-        );
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
